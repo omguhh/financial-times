5b9734d4895d2ad3aa892d97133c064ec74ae32d
Yasmine Fadel <yasmine.m.f@live.co.uk>
2021-10-22 12:55:34 +0400

    [HOTFIX] Fix heroku API key

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const compression = require('compression');
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
