102531ecacbd6cf3884e89059d7e936b76203f8a
Yasmine Fadel <yasmine.m.f@live.co.uk>
2021-10-22 12:33:11 +0400

    [FEAT] added eslint + jest config + updated travis yml

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+const createError = require('http-errors');
+const logger = require('morgan');
+const compression = require('compression');
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(compression());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Search' });
+  res.render('index');
 });
 
 app.listen(port, () => {
