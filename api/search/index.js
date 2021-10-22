e93a0dc51333f779f2d6442f1d063e65502ce9a9
Yasmine <yasmine.m.f@live.co.uk>
2021-10-22 16:32:57 +0400

    [FEAT] Add FT API and load search results (#6)

---
+++ api/search/index.js
@@ -0,0 +1,29 @@
+const http = require('../index');
+
+const search = async (term, page) => {
+  let searchResult = {
+    results: [],
+    page: 0,
+    total: 0,
+  };
+  try {
+    const response = await http.post('/content/search/v1', {
+      queryString: `title: ${term}`,
+      resultContext: {
+        offset: 1,
+        maxResults: 25,
+        aspects: ['title', 'lifecycle', 'location', 'editorial'],
+      },
+    });
+    const { data } = response;
+    searchResult.results = data.results[0].results;
+    searchResult.total = data.results[0].indexCount;
+    console.log('SUCCESS');
+  } catch (error) {
+    console.log('ERROR RESPONSE', error.response.data);
+    throw error;
+  }
+  return searchResult;
+};
+
+module.exports = search;
